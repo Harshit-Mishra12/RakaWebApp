d3e63ee3ae34d066ec1402b7384876e108194aa0
unknown <harshitmishra5348@gmail.com>
2024-06-24 13:10:06 +0530

    updated sql file

---
+++ eDoc.sql
@@ -0,0 +1,414 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2024 at 09:38 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eDoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `aemail` varchar(255) NOT NULL,
+  `apassword` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aemail`, `apassword`) VALUES
+('admin@edoc.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appoid` int(11) NOT NULL,
+  `pid` int(10) DEFAULT NULL,
+  `apponum` int(3) DEFAULT NULL,
+  `scheduleid` int(10) DEFAULT NULL,
+  `appodate` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`) VALUES
+(1, 1, 1, 1, '2022-06-03'),
+(4, 1, 2, 10, '2024-06-14'),
+(3, 1, 1, 10, '2024-06-14'),
+(5, 1, 3, 10, '2024-06-14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `docid` int(11) NOT NULL,
+  `docemail` varchar(255) DEFAULT NULL,
+  `docname` varchar(255) DEFAULT NULL,
+  `docpassword` varchar(255) DEFAULT NULL,
+  `docnic` varchar(15) DEFAULT NULL,
+  `doctel` varchar(15) DEFAULT NULL,
+  `specialties` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`docid`, `docemail`, `docname`, `docpassword`, `docnic`, `doctel`, `specialties`) VALUES
+(1, 'doctor@edoc.com', 'Test', '123', '000000000', '0110000000', 1),
+(2, 'starc@edoc.com', 'Starc', '123', '987898', '09305364771', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `email`, `address`, `city`, `state`, `country`, `postal_code`, `latitude`, `longitude`, `created_at`, `updated_at`) VALUES
+(4, 'admin@edoc.com', NULL, NULL, NULL, NULL, NULL, '26.45032960', '80.34713600', '2024-06-22 08:30:15', '2024-06-22 09:03:05'),
+(5, 'patient@edoc.com', NULL, NULL, NULL, NULL, NULL, '26.41888089', '80.31796957', '2024-06-22 08:31:07', '2024-06-22 08:31:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `pid` int(11) NOT NULL,
+  `pemail` varchar(255) DEFAULT NULL,
+  `pname` varchar(255) DEFAULT NULL,
+  `ppassword` varchar(255) DEFAULT NULL,
+  `paddress` varchar(255) DEFAULT NULL,
+  `pnic` varchar(15) DEFAULT NULL,
+  `pdob` date DEFAULT NULL,
+  `ptel` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`pid`, `pemail`, `pname`, `ppassword`, `paddress`, `pnic`, `pdob`, `ptel`) VALUES
+(1, 'patient@edoc.com', 'Test Patient', '123', 'Sri Lanka', '0000000000', '2000-01-01', '0120000000'),
+(2, 'emhashenudara@gmail.com', 'Hashen Udara', '123', 'Sri Lanka', '0110000000', '2022-06-03', '0700000000'),
+(3, 'patientTest@edoc.com', 'harshit mishra', '123456', 'new york', '76567', '2024-06-20', '0120000000'),
+(4, 'docTest@edoc.com', 'steve warner', '123456', 'white field', '987678', '1995-10-19', '0120000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `scheduleid` int(11) NOT NULL,
+  `docid` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `scheduledate` date DEFAULT NULL,
+  `nop` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`scheduleid`, `docid`, `title`, `scheduledate`, `nop`) VALUES
+(1, '2', 'test', '2024-06-20', 40),
+(2, '2', 'test2', '2024-06-20', 40),
+(8, '2', 'test46', '2024-06-21', 40),
+(9, '1', 'Main Emergency Session', '2024-07-01', 20),
+(10, '2', 'New Session', '2024-06-26', 20),
+(11, '2', 'test50', '2024-06-26', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule_slots`
+--
+
+CREATE TABLE `schedule_slots` (
+  `id` int(11) NOT NULL,
+  `scheduleid` int(11) NOT NULL,
+  `starttime` varchar(255) DEFAULT NULL,
+  `endtime` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `schedule_slots`
+--
+
+INSERT INTO `schedule_slots` (`id`, `scheduleid`, `starttime`, `endtime`, `created_at`, `updated_at`) VALUES
+(1, 1, '17:26', '18:27', '2024-06-19 04:27:04', '2024-06-19 04:27:04'),
+(2, 2, '17:26', '18:27', '2024-06-19 05:24:32', '2024-06-19 05:24:32'),
+(3, 8, '16:30', '18:30', '2024-06-19 06:30:57', '2024-06-19 06:30:57'),
+(5, 8, '19:30', '20:30', '2024-06-19 06:30:57', '2024-06-19 06:30:57'),
+(6, 9, '19:48', '22:48', '2024-06-19 07:48:36', '2024-06-19 07:48:36'),
+(7, 10, '17:11', '18:12', '2024-06-19 08:12:39', '2024-06-19 08:12:39'),
+(8, 11, '16:31', '17:31', '2024-06-24 03:32:45', '2024-06-24 03:32:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `id` int(2) NOT NULL,
+  `sname` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `specialties`
+--
+
+INSERT INTO `specialties` (`id`, `sname`) VALUES
+(1, 'Accident and emergency medicine'),
+(2, 'Allergology'),
+(3, 'Anaesthetics'),
+(4, 'Biological hematology'),
+(5, 'Cardiology'),
+(6, 'Child psychiatry'),
+(7, 'Clinical biology'),
+(8, 'Clinical chemistry'),
+(9, 'Clinical neurophysiology'),
+(10, 'Clinical radiology'),
+(11, 'Dental, oral and maxillo-facial surgery'),
+(12, 'Dermato-venerology'),
+(13, 'Dermatology'),
+(14, 'Endocrinology'),
+(15, 'Gastro-enterologic surgery'),
+(16, 'Gastroenterology'),
+(17, 'General hematology'),
+(18, 'General Practice'),
+(19, 'General surgery'),
+(20, 'Geriatrics'),
+(21, 'Immunology'),
+(22, 'Infectious diseases'),
+(23, 'Internal medicine'),
+(24, 'Laboratory medicine'),
+(25, 'Maxillo-facial surgery'),
+(26, 'Microbiology'),
+(27, 'Nephrology'),
+(28, 'Neuro-psychiatry'),
+(29, 'Neurology'),
+(30, 'Neurosurgery'),
+(31, 'Nuclear medicine'),
+(32, 'Obstetrics and gynecology'),
+(33, 'Occupational medicine'),
+(34, 'Ophthalmology'),
+(35, 'Orthopaedics'),
+(36, 'Otorhinolaryngology'),
+(37, 'Paediatric surgery'),
+(38, 'Paediatrics'),
+(39, 'Pathology'),
+(40, 'Pharmacology'),
+(41, 'Physical medicine and rehabilitation'),
+(42, 'Plastic surgery'),
+(43, 'Podiatric Medicine'),
+(44, 'Podiatric Surgery'),
+(45, 'Psychiatry'),
+(46, 'Public health and Preventive Medicine'),
+(47, 'Radiology'),
+(48, 'Radiotherapy'),
+(49, 'Respiratory medicine'),
+(50, 'Rheumatology'),
+(51, 'Stomatology'),
+(52, 'Thoracic surgery'),
+(53, 'Tropical medicine'),
+(54, 'Urology'),
+(55, 'Vascular surgery'),
+(56, 'Venereology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webuser`
+--
+
+CREATE TABLE `webuser` (
+  `email` varchar(255) NOT NULL,
+  `usertype` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `webuser`
+--
+
+INSERT INTO `webuser` (`email`, `usertype`) VALUES
+('admin@edoc.com', 'a'),
+('doctor@edoc.com', 'd'),
+('patient@edoc.com', 'p');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aemail`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appoid`),
+  ADD KEY `pid` (`pid`),
+  ADD KEY `scheduleid` (`scheduleid`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`docid`),
+  ADD KEY `specialties` (`specialties`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`scheduleid`);
+
+--
+-- Indexes for table `schedule_slots`
+--
+ALTER TABLE `schedule_slots`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `scheduleid` (`scheduleid`);
+
+--
+-- Indexes for table `specialties`
+--
+ALTER TABLE `specialties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `webuser`
+--
+ALTER TABLE `webuser`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `scheduleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `schedule_slots`
+--
+ALTER TABLE `schedule_slots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`email`) REFERENCES `webuser` (`email`);
+
+--
+-- Constraints for table `schedule_slots`
+--
+ALTER TABLE `schedule_slots`
+  ADD CONSTRAINT `schedule_slots_ibfk_1` FOREIGN KEY (`scheduleid`) REFERENCES `schedule` (`scheduleid`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
